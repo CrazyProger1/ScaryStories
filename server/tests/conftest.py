bcc198e250512c20d6ea5ee2a3acc661c1e22fbe
CrazyProger1 <crazyproger1@gmail.com>
2023-08-31 15:48:30 +0300

    Add: register test

---
+++ server/tests/conftest.py
@@ -0,0 +1,56 @@
+import asyncio
+from typing import AsyncGenerator
+
+import pytest
+from fastapi.testclient import TestClient
+from httpx import AsyncClient
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
+
+from src.database import get_async_session
+from src.database import metadata
+from src.config import settings
+from main import app
+
+# DATABASE
+DATABASE_URL_TEST = f'postgresql+asyncpg://{settings.db_user}:{settings.db_pass}' \
+                    f'@{settings.db_host}:{settings.db_port}/{settings.db_name}'
+
+engine_test = create_async_engine(DATABASE_URL_TEST, poolclass=NullPool)
+async_session_maker = sessionmaker(engine_test, class_=AsyncSession, expire_on_commit=False)
+metadata.bind = engine_test
+
+
+async def override_get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+app.dependency_overrides[get_async_session] = override_get_async_session
+
+
+@pytest.fixture(autouse=True, scope='session')
+async def prepare_database():
+    async with engine_test.begin() as conn:
+        await conn.run_sync(metadata.create_all)
+    yield
+    async with engine_test.begin() as conn:
+        await conn.run_sync(metadata.drop_all)
+
+
+# SETUP
+@pytest.fixture(scope='session')
+def event_loop(request):
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
+
+
+client = TestClient(app)
+
+
+@pytest.fixture(scope='session')
+async def ac() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(app=app, base_url='http://test') as ac:
+        yield ac
---
+++ server/tests/test_auth.py
@@ -0,0 +1,14 @@
+from conftest import client
+
+
+def test_register():
+    response = client.post("/users/register", json={
+        'email': 'user@example.com',
+        'password': 'string',
+        'is_active': True,
+        'is_superuser': False,
+        'is_verified': False,
+        'username': 'string'
+    })
+
+    assert response.status_code == 201
