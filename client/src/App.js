7331aad90a13248712cd5beff75f74e5bc6427cd
CrazyProger1 <crazyproger1@gmail.com>
2023-11-21 17:21:27 +0200

    Add: main font

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.css";
+import "./styles/Fonts.css";
 import MainPage from "./components/pages/MainPage";
 import CategoriesPage from "./components/pages/CategoriesPage";
 import AboutPage from "./components/pages/AboutPage";
--- client/src/components/pages/AboutPage.jsx
+++ client/src/components/pages/AboutPage.jsx
@@ -1,34 +1,58 @@
 import React from 'react';
 import PageWrapper from "./PageWrapper";
 import {Image, Stack} from "react-bootstrap";
+// import facebook from '../../../public/imgs/icons/facebook.png';
 
 const AboutPage = () => {
     return (
         <PageWrapper>
             <div>
-                <h1 className="text-center">About</h1>
-                <p>Scary Stories is a platform for... It designed... Bla bla bla...</p>
-                <h2 className="text-center">Meet the team</h2>
+                <h1 className="text-center mt-5">About</h1>
+                <p className="mt-5">Scary Stories is a platform for sharing scary stories. Bla bla bla bla bla bla bla
+                    bla bla bla bla
+                    bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
+                    bla bla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla blabla bla bla bla bla bla
+                    bla bla blabla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla blabla bla bla bla bla
+                    bla bla bla blabla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla blabla bla bla bla
+                    bla bla bla bla blabla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla blabla bla bla
+                    bla bla bla bla bla blabla bla bla bla bla bla bla bla blabla bla bla bla bla bla bla bla blabla bla
+                    bla bla bla bla bla bla bla</p>
 
-                <Stack direction="horizontal" gap={2}>
-                    <div>
-                        <h3>Nikolay Hetman</h3>
-                        <h5>Developer In Chief</h5>
-                        <p/>
-                        <p>I'm the Python Developer... Bla bla bla I'm the Python Developer... Bla bla bla I'm the Python Developer... Bla bla bla</p>
-                        <p>I'm the Python Developer... Bla bla bla</p>
-                        <p>I'm the Python Developer... Bla bla bla</p>
-                        <p>I'm the Python Developer... Bla bla bla</p>
-                        <p>I'm the Python Developer... Bla bla bla</p>
-                        <p>I'm the Python Developer... Bla bla bla</p>
-                    </div>
 
-                    <Image
-                        className="rounded-circle ms-auto"
-                        src="https://images.techinsider.ru/upload/img_cache/b41/b41448dba630129d02ca5dfaad0fc5b7_cropped_666x666.jpg"
-                        width="200px"
-                    />
-                </Stack>
+                <h2 className="text-center" style={{marginTop: "10rem"}}>Meet the team</h2>
+
+                <div className="align-content-center">
+                    <Stack className="mt-5" direction="horizontal" gap={3}>
+                        <Image
+                            className="rounded-circle"
+                            src="https://cdn.pixabay.com/photo/2020/05/09/21/43/hacker-5151533_1280.jpg"
+                            width="200px"
+                        />
+                        <div>
+                            <h3>Nikolay Hetman</h3>
+                            <h5>Developer In Chief</h5>
+                            <p/>
+                            <p>Hi, I'm the Python Developer.</p>
+                            <Stack gap={1} direction="horizontal">
+                                <a href="https://www.facebook.com/yourpage" target="_blank">
+                                    <img src={process.env.PUBLIC_URL + '/imgs/icons/instagram.png'} alt="Instagram"
+                                         width="40px"/>
+                                </a>
+                                <a href="https://www.facebook.com/yourpage" target="_blank">
+                                    <img src={process.env.PUBLIC_URL + '/imgs/icons/linkedin.png'} alt="Linkedin"
+                                         width="40px"/>
+                                </a>
+                                <a href="https://www.facebook.com/yourpage" target="_blank">
+                                    <img src={process.env.PUBLIC_URL + '/imgs/icons/telegram.png'} alt="Telegram"
+                                         width="40px"/>
+                                </a>
+                            </Stack>
+                        </div>
+
+
+                    </Stack>
+                </div>
+
 
 
             </div>
